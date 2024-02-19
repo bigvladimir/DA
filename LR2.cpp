687e2811bb307a4b5e26530f818d6556fa0e679b
Харченко Владимир <69915638+bigvladimir@users.noreply.github.com>
2024-02-19 05:09:12 +0300

    Update LR2.cpp

--- LR2.cpp
+++ LR2.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 #include <stdlib.h>
 #include <ctype.h>
+#include <random>
+#include <chrono>
 #include <string.h>
+
+using namespace std;
+
+std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
 
 using ull = unsigned long long;
 
@@ -11,22 +17,21 @@ class Treap {
 private:
   struct Node;
   struct Treap_pair;
-  struct remove_info;
 
   Node *root;
 
   void kill(Node *treap_pointer);
   Treap_pair split(Node *treap, char *key);
   Node *merge(Treap_pair p);
+  Node **search_remove(Node **treap, char *key);
   Node *search(Node *treap, char *key);
-  remove_info search_remove(Node *treap, char *key);
 public:
   Treap();
   ~Treap();
 
-  int insert(char *key, ull value);
+  bool insert(char *key, ull value);
 
-  int remove(char *key);
+  bool remove(char *key);
 
   struct find_output;
   find_output find(char *key);
@@ -36,37 +41,32 @@ public:
 
 struct Treap::Node {
   char *key;
-  int priority;
+  uint_fast32_t priority;
   ull value;
   Node *left, *right;
-  Node(char *input_key, ull input_value);
+  Node(char *_key, ull _value, uint_fast32_t _priority, Treap_pair p);
   ~Node();
 };
 
-Treap::Node::Node(char *input_key, ull input_value) {
+struct Treap::Treap_pair {
+  Node *left, *right;
+};
+
+Treap::Node::Node(char *_key, ull _value, uint_fast32_t _priority, Treap_pair p) {
   key = new char[257];
-  for(int i = 0; i < 257; ++i) key[i] = input_key[i];
-  priority = rand();
-  value = input_value;
-  left = nullptr;
-  right = nullptr;
+  for(int i = 0; i < 257; ++i) key[i] = _key[i];
+  priority = _priority;
+  value = _value;
+  left = p.left;
+  right = p.right;
 }
 
 Treap::Node::~Node() {
   delete[] key;
 }
 
-struct Treap::Treap_pair {
-  Node *left, *right;
-};
-
-struct Treap::remove_info {
-  Node *res, *prev;
-  int l;
-};
-
 struct Treap::find_output {
-  int result;
+  bool result;
   ull value;
 };
 
@@ -77,7 +77,7 @@ Treap::Treap() {
 }
 
 Treap::Treap_pair Treap::split(Node *treap, char *key) {
-  if (!treap) {
+  if (treap == nullptr) {
     return {nullptr, nullptr};
   } else if (strcmp(key, treap->key) > 0) {
     Treap_pair t = split(treap->right, key);
@@ -91,9 +91,9 @@ Treap::Treap_pair Treap::split(Node *treap, char *key) {
 }
 
 Treap::Node *Treap::merge(Treap_pair p) {
-  if (!p.right) {
+  if (p.right == nullptr) {
     return p.left;
-  } else if (!p.left) {
+  } else if (p.left == nullptr) {
     return p.right;
   } else if (p.left->priority > p.right->priority) {
     p.left->right = merge({p.left->right, p.right});
@@ -104,81 +104,82 @@ Treap::Node *Treap::merge(Treap_pair p) {
   }
 }
 
-Treap::Node *Treap::search(Node *treap, char *key) {
-  while(treap) {
-    int result = strcmp(key, treap->key);
-    if (result == 0) {
-      return treap;
-    } else if (result < 0) {
-      treap = treap->left;
+bool Treap::insert(char *key, ull value) {
+  uint_fast32_t priority = rng();
+  Node **t_treap = &root;
+  while(*t_treap != nullptr && (*t_treap)->priority >= priority) { // !!! >
+    int result = strcmp(key, (*t_treap)->key);
+    if (result < 0) {
+      t_treap = &(*t_treap)->left;
+    } else if (result > 0) {
+      t_treap = &(*t_treap)->right;
     } else {
-      treap = treap->right;
+      return false;
+    }
+  }
+  Node *try_treap = *t_treap;
+  while(try_treap != nullptr) {
+    int result = strcmp(key, try_treap->key);
+    if (result < 0) {
+      try_treap = try_treap->left;
+    } else if (result > 0) {
+      try_treap = try_treap->right;
+    } else {
+      return false;
+    }
+  }
+  *t_treap = new Node(key, value, priority, split(*t_treap, key));
+  return true;
+}
+
+Treap::Node **Treap::search_remove(Node **treap, char *key) {
+  while(*treap != nullptr) {
+    int result = strcmp(key, (*treap)->key);
+    if (result < 0) {
+      treap = &(*treap)->left;
+    } else if (result > 0) {
+      treap = &(*treap)->right;
+    } else {
+      return treap;
     }
   }
   return treap;
 }
 
-int Treap::insert(char *key, ull value) {
-  if (!search(root, key)) {
-    Treap_pair p = split(root, key);
-    p.left = merge({p.left, new Node(key, value)});
-    root = merge(p);
-    return 1;
+bool Treap::remove(char *key) {
+  Node **p = search_remove(&root, key);
+
+  if (*p != nullptr) {
+    Node *n_p = merge({(*p)->left, (*p)->right});
+    delete *p;
+    *p = n_p;
+    
+    return true;
   } else {
-    return 0;
+    return false;
   }
 }
 
-Treap::remove_info Treap::search_remove(Node *treap, char *key) {
-  Node *prev = nullptr;
-  int l;
-  while(treap) {
+Treap::Node *Treap::search(Node *treap, char *key) {
+  while(treap != nullptr) {
     int result = strcmp(key, treap->key);
-    if (result == 0) {
-      return {treap, prev, l};
-    } else if (result < 0) {
-      prev = treap;
-      l = 1;
+    if (result < 0) {
       treap = treap->left;
-    } else {
-      prev = treap;
-      l = 0;
+    } else if (result > 0){
       treap = treap->right;
-    }
-  }
-  return {treap, prev, l};
-}
-
-int Treap::remove(char *key) {
-  remove_info p = search_remove(root, key);
-  
-  if (p.res) {
-    if (p.prev) {
-      Node *n_p = merge({p.res->left, p.res->right});
-      if (p.l) {
-        delete p.res;
-        p.prev->left = n_p;
-      } else {
-        delete p.res;
-        p.prev->right = n_p;
-      }
     } else {
-      Node *n_p = merge({p.res->left, p.res->right});
-      delete root;
-      root = n_p;
+      return treap;
     }
-    return 1;
-  } else {
-    return 0;
   }
+  return treap;
 }
 
 Treap::find_output Treap::find(char *key) {
   Node *result = search(root, key);
-  if (result) {
-    return {1, result->value};
+  if (result != nullptr) {
+    return {true, result->value};
   } else {
-    return {0, 0};
+    return {false, 0};
   }
 }
 
@@ -203,6 +204,9 @@ char *to_lower(char *input) {
 }
 
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  
   Treap *treap = new Treap;
 
   char input[257];
@@ -236,6 +240,6 @@ int main() {
   }
 
   delete treap;
-  
+
   return 0;
 }
